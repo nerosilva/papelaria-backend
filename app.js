4dc2f87fb9f1049c775f7dbe1a1d8b12220b9fea
Informática para Internet <nerosilva>
2024-02-21 21:38:39 -0300

    iniciamos uma rota

--- app.js
+++ app.js
@@ -1,10 +1,61 @@
 const express = require('express')
 const app = express();
-
-app.use((req,res,next)=>{
-    res.json({
-        mensagem:"Olá Mundo!"
-    })
+const usuario = [
+    {
+        id: 1,
+        nome: "Nero",
+        email: "nero@gmail.com",
+        senha: "1012"
+    },
+    {
+        id: 2,
+        nome: "Silva",
+        email: "silva@gmail.com",
+        senha: "1012"
+    },
+    {
+        id: 3,
+        nome: "Cauê",
+        email: "cauê@gmail.com",
+        senha: "1012"
+    },
+    {
+        id: 4,
+        nome: "Carlos",
+        email: "carlos@gmail.com",
+        senha: "1012"
+    },
+]
+app.get("/", (req, res, next) => {
+    res.json(usuario)
 })
+
+app.get("/usuario", (req, res, next) => {
+    let nomes = [];
+    usuario.map((linha) => {
+        nomes.push({
+            nome: linha.nome,
+            email: linha.email
+
+        })
+    })
+
+    res.json(nomes)
+
+})
+app.post("/usuario", (req, res, next) => {
+    const id = req.body.id
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const senha = req.body.senha;
+    const dados = [{
+        id,
+        nome,
+        email,
+        senha
+    }]
+    console.table(dados);
+
+});
 
 module.exports = app
